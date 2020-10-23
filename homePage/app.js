ff4c68be6df995368050c9e26a111c54daba03b1
Oluwaseyigit <64203917+Oluwaseyigit@users.noreply.github.com>
2020-10-23 21:25:34 +0100

    Update app.js

--- homePage/app.js
+++ homePage/app.js
@@ -1,8 +1,30 @@
-const btn = document.querySelector('.test')
-btn.addEventListener('click', justWork)
 
 
-function justWork(){
-    console.log('worked')
-    alert('worked')
-}
+(function($) { 
+    $(function() { 
+      $('nav ul li a:not(:only-child)').click(function(e) {
+        $(this).siblings('.nav-dropdown').toggle();
+        $('.dropdown').not($(this).siblings()).hide();
+        e.stopPropagation();
+      });
+      $('html').click(function() {
+        $('.nav-dropdown').hide();
+      });
+      $('#nav-toggle').click(function() {
+        $('nav ul').slideToggle();
+      });
+      $('#nav-toggle').on('click', function() {
+        this.classList.toggle('active');
+      });
+    }); 
+  })(jQuery);
+
+  let image = document.images[1]
+  let bigImage = document.createElement('img')
+
+  bigImage.onload = function(){
+    image.src = this.src
+  }
+  setTimeout(function(){
+    bigImage.src = 'images/dinner.jpg'
+  },50)
